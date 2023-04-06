2d9e6abc97d7cd61d877eeadc34b1274f8ab63a9
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-04-06 16:15:52 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Design Patterns using C#!!");
+        Console.WriteLine();
+
+        Console.WriteLine("Most useful patterns in C#");
+        Console.WriteLine();
+        Console.WriteLine("1. Builder Design Pattern and Fluent Builder");
+    }
+}
